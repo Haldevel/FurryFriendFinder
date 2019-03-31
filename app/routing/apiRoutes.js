ceaf0cc91febb5353c65b1e1ebdac8dc952e1b2b
Susie <45341415+Haldevel@users.noreply.github.com>
2019-03-31 11:50:24 -0700

    done cleaning up apiRoutes file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,42 +18,44 @@ module.exports = function (app) {
         // req.body is available since we're using the body parsing middleware
         console.log("Req body: " + req.body);
 
+        //we received an object with the data filled out by a user it is an object which has 'scores' array with the answer keys
         var userObject = req.body;
         var userScores = userObject.scores;
 
+        //this array will contain the data of the top three matches
         var finalThreeArray = [];
 
+        //the map will hold the keys which correspond the number (index) of the dog in the smalDogs array,
+        // and the values will be the numbers reflecting the difference between scores for a user and a dog
         var myMap = new Map();
 
+        //this array will hold an array of top three dog best matching object 
+        var threeMatches = [];
 
-        console.log("userScores: " + userScores);
+        /* console.log("userScores: " + userScores);
         for (var i = 0; i < userScores.length; i++) {
             console.log(userScores[i]);
-        }
+        } */
 
+        //iterate through the dogs array to get scores for each dog
         for (var j = 0; j < smallDogs.length; j++) {
             console.log("dogs scores: " + smallDogs[j].scores);
             var dogDiff = 0;
             for (var i = 0; i < userScores.length; i++) {
-
+                //calculate the difference between a user and a dog scores
                 dogDiff = dogDiff + Math.abs(userScores[i] - smallDogs[j].scores[i]);
 
-
             }
-            // differArr.push({[j]: dogDiff });
+            //set the map key-value pairs - the keys are the order/index 
+            //of a dog in the smallDogs array, the value is the difference in scores
             myMap.set(j, dogDiff);
 
         }
 
-        //console.log("differArr: " + differArr[0].);
-
-        for (var [key, value] of myMap) {
-            console.log(key + ' = ' + value);
-        }
-
+        //sort the map by its values from the smallest to the biggest difference
         const mapSort1 = new Map([...myMap.entries()].sort((a, b) => a[1] - b[1]));
-        console.log(mapSort1);
 
+        //loop through the sorted by value map, and take top three elements and push them into finalThreeArray
         var count = 0;
         for (var [key, value] of mapSort1) {
             console.log(key + ' = ' + value);
@@ -64,9 +66,10 @@ module.exports = function (app) {
             else break;
         }
 
-        console.log(finalThreeArray);
-        var threeMatches = [];
-
+        //console.log(finalThreeArray);
+  
+        //populate threeMatches array with the dog objects containg the properties 'breed' and 'image' 
+        //the array will be sent in the response object
         for(var i = 0; i < finalThreeArray.length; i++) {
             console.log(smallDogs[finalThreeArray[i]].breed);
             threeMatches.push({"breed": smallDogs[finalThreeArray[i]].breed, "image": smallDogs[finalThreeArray[i]].photo});
@@ -75,17 +78,5 @@ module.exports = function (app) {
         res.json(threeMatches);
         
     });
-
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-
-    app.post("/api/clear", function (req, res) {
-        // Empty out the arrays of data
-        tableData = [];
-        waitListData = [];
-
-        res.json({ ok: true });
-
-    });
+ 
 };
