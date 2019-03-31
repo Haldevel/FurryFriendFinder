dd83b497ece1c97a2273701798d353f5251cd6f3
Susie <45341415+Haldevel@users.noreply.github.com>
2019-03-31 11:33:29 -0700

    start cleaning up apiRoutes file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,17 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
+//Linking our routes to a data source (the array of objects)
 var smallDogs = require("../data/dogs");
-//var waitListData = require("../data/waitinglistData");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
+//routing
 module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-
+    // API GET Request to get an array of small dogs objects
      app.get("/api/furryfriends", function(req, res) {
       res.json(smallDogs);
     });
   
-    /*
-    app.get("/api/waitlist", function(req, res) {
-      res.json(waitListData);
-    }); */
 
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-
+    // API POST Request
+    // Below code handles when a user submits the survey form which triggers calculations of the top three matches for the user
     app.post("/api/furryfriends", function (req, res) {
         // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
         // It will do this by sending out the value "true" have a table
